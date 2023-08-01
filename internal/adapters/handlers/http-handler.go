0bdb3aa1506aba03788e52bb3c73ad1e3aca39d5
[FadyGamilM] <1700961@eng.asu.edu.eg>
2023-08-01 05:28:52 +0300

    FEATURE | start working with mongodb

--- internal/adapters/handlers/http-handler.go
+++ internal/adapters/handlers/http-handler.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/FadyGamilM/hotelreservationapi/internal/adapters/repository/mongo"
+	"github.com/gofiber/fiber/v2"
+)
 
 type HttpHandler struct {
 	// should depends on the service port which is the input ports which is defined in the core/ports/input folder
@@ -13,4 +16,9 @@ func NewHttpHandler() (*HttpHandler, error) {
 
 func (h *HttpHandler) TestReadinessHandler(ctx *fiber.Ctx) error {
 	return ctx.JSON(map[string]string{"resp": "working just fine !"})
+}
+
+func (h *HttpHandler) CreateUser(ctx *fiber.Ctx) error {
+	mongodb := mongo.NewMongoDB("mongodb://localhost:27017")
+	return ctx.JSON("inserted")
 }
--- internal/adapters/repository/mongo/dtos/user-dto.go
+++ internal/adapters/repository/mongo/dtos/user-dto.go
@@ -1,7 +1,9 @@
 package dtos
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type UserDto struct {
-	ID        string `bson:"_id" json:"id"`
-	FirstName string `bson:"first_name" json:"first_name"`
-	LastName  string `bson:"last_name" json:"last_name"`
+	ID        primitive.ObjectID `bson:"_id" json:"id"`
+	FirstName string             `bson:"first_name" json:"first_name"`
+	LastName  string             `bson:"last_name" json:"last_name"`
 }
